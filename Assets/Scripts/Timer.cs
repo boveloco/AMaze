26a430c4e4422977723b1eee4b83deaff389b1f3
André Rennó Bovendorp <andre.bovendorp@pucpr.edu.br>
2017-11-07 20:04:48 -0200

    HUD

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.UI;
+using UnityEngine;
+
+class Timer : MonoBehaviour
+{ 
+    public float startTime;
+    private Text text;
+    void Start()
+    {
+        text = GetComponent<Text>();
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("F0");
+        text.text = minutes + ":" + seconds;
+        
+        
+    }
+
+}
